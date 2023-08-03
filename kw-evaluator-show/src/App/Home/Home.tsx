38058558538269f32623a324921b62812ab329c3
HRan2004 <2125764918@qq.com>
2023-08-03 18:07:39 +0800

    feat: 初步添加图表

--- kw-evaluator-show/src/App/Home/Home.tsx
+++ kw-evaluator-show/src/App/Home/Home.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import Style from './Home.module.scss'
+import { Chart } from '@antv/g2'
+import MainTable from "./MainTable/MainTable";
 
 function Home() {
+  const tableContainerRef = useRef(null)
+
+  useEffect(() => {
+
+  }, [])
+
   return (
     <div className={Style.Home}>
       <div className={Style.headline}></div>
       <div className={Style.headlineHover}></div>
       <div className={Style.content}>
-        <div className={Style.evaluator}></div>
+        <div className={Style.evaluator}>
+          <MainTable/>
+        </div>
         <div className={Style.chat}></div>
       </div>
       <div className={Style.footer}></div>
