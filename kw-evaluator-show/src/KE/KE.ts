483d98862edd87188895a3b8df720989c7842504
HRan2004 <2125764918@qq.com>
2023-08-31 18:20:54 +0800

    refactor: 还原KE渲染流程

--- kw-evaluator-show/src/KE/KE.ts
+++ kw-evaluator-show/src/KE/KE.ts
@@ -24,65 +24,13 @@ export default class KE {
   }
 
   static render(canvas: HTMLCanvasElement): void {
-    console.log('render')
     this.initEngine(canvas)
 
-    KE.scene.clearColor = new Color4(0.9, 0.9, 0.9, 1)
+    Environment.init()
+    GUI.init()
 
-    const camera = new ArcRotateCamera(
-      "Main Camera",
-      Math.PI / 4,
-      Math.PI / 2.5,
-      7,
-      new Vector3(0, 0.6, 0),
-      KE.scene
-    )
-    camera.attachControl(canvas, true);
-    camera.lowerRadiusLimit = 0.005
-    camera.upperRadiusLimit = 200
-    camera.lowerBetaLimit = 0.2
-    camera.upperBetaLimit = 3.13
+    Builder.build()
 
-    // camera.wheelDeltaPercentage = 0.5
-    camera.pinchDeltaPercentage = 0.02
-    camera.useBouncingBehavior = true
-    camera.useNaturalPinchZoom = true
-
-    const light = new HemisphericLight("light1", new Vector3(12, 16, 6), KE.scene)
-    light.intensity = 1.0
-    light.groundColor = new Color3(0.8, 0.8, 0.8)
-    light.diffuse = new Color3(0.97, 0.97, 0.97)
-
-    const addData = (x: number, y: number, h: number) => {
-      const scale = 0.1
-      const size = 0.6
-      const sh = 2
-
-      const material = new GradientMaterial("grad", KE.scene);
-      material.topColor = new Color3(1.0, 1.0, 1.0); // Set the gradient top color
-      material.bottomColor = new Color3(0.4, 0.4, 0.4); // Set the gradient bottom color
-      material.offset = 0.6;
-
-      const box = CreateBox('box1')
-      box.material = material
-      box.scaling = new Vector3(size * scale, h * sh, size * scale)
-      box.position = new Vector3(x * scale, h / 2 * sh, y * scale)
-    }
-
-    result.map((line, i) => {
-      let j = 0
-      for (const k in line.score_level_2) {
-        // @ts-ignore
-        let v = line.score_level_2[k] * 1.2 - 0.1
-        addData(i, j, v)
-        j++
-      }
-    })
-
-    // Environment.init()
-    // GUI.init()
-
-    // Builder.build()
     Debugger.startDebug()
   }
 
