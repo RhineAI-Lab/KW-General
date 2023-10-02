bf1f14c5dcf516e873bd83b491aad59b391846bb
HRan2004 <2125764918@qq.com>
2023-10-02 11:19:11 +0800

    feat: 新增CuteGPT前端项目

---
+++ cutegpt-frontend/src/App/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Style from './App.module.scss'
+import M3Style from './Material3Color.module.scss'
+import {Route, Routes} from "react-router-dom";
+import Home from "./Home/Home";
+
+function App() {
+  return (
+    <div className={Style.App}>
+      <div className={M3Style.MioRoot}>
+
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    // eslint-disable-next-line
+    interface IntrinsicElements {
+      [tag: string]: any
+    }
+  }
+}
---
+++ cutegpt-frontend/src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App/App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import {material2Theme} from "./App/Material2Theme";
+import {ThemeProvider} from "@mui/material";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={material2Theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
